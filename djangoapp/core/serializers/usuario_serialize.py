267266db56db0f860fcff460dd7a36e8c1d3d487
Vinicius de Oliveira <viniciusoliveira926@gmail.com>
2023-11-06 23:30:38 +0000

    bug: Correcao Serializers

---
+++ djangoapp/core/serializers/usuario_serialize.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from ..models.usuario_model import Usuario
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'username', 'email', 'primeiro_nome', 
+                  'segundo_nome', 'data_nascimento', 'senha', 
+                  'e_admin', 'criado_em', 'atualizado_em']
