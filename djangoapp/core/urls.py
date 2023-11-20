1adc236f9e42e670d486487035e116b46800f72c
Vinicius de Oliveira <viniciusoliveira926@gmail.com>
2023-11-20 20:21:52 -0300

    feat:Adicionando Comp no projeto (#3)

--- djangoapp/core/urls.py
+++ djangoapp/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from .views.usuario_view import UsuarioView
+from .views.compilador_view import ExecuteCodeView
 
 urlpatterns = [
     path('usuarios/', UsuarioView.as_view(), name='usuario_list'),
     path('usuarios/<int:pk>/', UsuarioView.as_view(), name='usuario_detail'),
+    path('execute-code/', ExecuteCodeView.as_view(), name='execute-code'),
 ]
