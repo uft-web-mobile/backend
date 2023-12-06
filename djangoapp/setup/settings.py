38861f6bd83c5f340cfc16196c233797fb4a1bef
vinicius <viniciusoliveira926@gmail.com>
2023-12-06 13:25:29 -0300

    feat: Config Cors

--- djangoapp/setup/settings.py
+++ djangoapp/setup/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -174,3 +175,8 @@ LOGGING = {
 }
 
 AUTH_USER_MODEL = 'core.Usuario'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",# Substitua pelo domínio de origem do seu aplicativo React
+]
